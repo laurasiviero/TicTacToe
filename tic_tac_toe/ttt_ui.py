dedac334c592d1965a62b440bab22fb5258c67a7
laurasiviero <82674985+laurasiviero@users.noreply.github.com>
2021-05-07 00:20:21 +0200

    Update ttt_ui.py

--- tic_tac_toe/ttt_ui.py
+++ tic_tac_toe/ttt_ui.py
@@ -24,7 +24,6 @@ Description: - This tool allows you to play tic-tac-toe on maya(python 2.7);
 import sys
 import maya.cmds as cmds
 
-
 import ttt_enemy_functions as ttt
 
 
@@ -32,7 +31,6 @@ def tic_tac_toe_ui(USERPATH):
     PATH_ICONS = USERPATH + "\\tic_tac_toe_icons\\"
     sys.path.append(USERPATH)
     sys.path.append(PATH_ICONS)
-    print("directories have been updated")
 
     ui_title = "tic tac_toe"
     main_color = [0.492, 0.980, 0.570]
@@ -47,25 +45,31 @@ def tic_tac_toe_ui(USERPATH):
     if cmds.window(ui_title, exists=True):
         cmds.deleteUI(ui_title)
 
-
     ###############################################################
     ##########################   HEADER   #########################
     ###############################################################
     cmds.columnLayout(rowSpacing=5)
 
+
     cmds.formLayout("ttt_form_count", backgroundColor=main_color,
                     numberOfDivisions=100)
-
+    
     ttt_count_user = cmds.image("ttt_count_user",
-                                image=PATH_ICONS + "ttt_number\\" + 'ttt_0.png')
+                                image=PATH_ICONS + "\\ttt_number\\" + 'ttt_0.png')
 
     ttt_count_ia = cmds.image("ttt_count_ia",
-                              image=PATH_ICONS + "ttt_number\\" + 'ttt_0.png')
+                              image=PATH_ICONS + "\\ttt_number\\" + 'ttt_0.png')
     cmds.image("ttt_bg", image=PATH_ICONS + 'ttt_banner.png')
+    
+    ttt_difficulty = cmds.iconTextButton("ttt_difficulty", style='iconOnly',
+                                       image=PATH_ICONS + 'ttt_settings_fair.png',
+                                       command="ttt.change_difficulty(USERPATH)")
 
+    
     cmds.formLayout("ttt_form_count", edit=True,
-                    attachForm=[(ttt_count_user, 'left', 70), (ttt_count_user, 'top', 88),
-                                (ttt_count_ia, 'left', 385), (ttt_count_ia, 'top', 88)])
+         attachForm=[(ttt_count_user, 'left', 70), (ttt_count_user, 'top', 88),
+                    (ttt_count_ia, 'left', 385), (ttt_count_ia, 'top', 88),
+                    (ttt_difficulty, 'left', 420), (ttt_difficulty, 'top', 0)])
     cmds.setParent("..")
 
 
@@ -78,43 +82,44 @@ def tic_tac_toe_ui(USERPATH):
 
     ttt_button01 = cmds.iconTextButton("ttt_button01", style='iconOnly',
                                        image=PATH_ICONS + 'ttt_idle.png',
-                                       command="ttt.set_turn('ttt_button01', USERPATH)")
+                                       command="ttt.check_difficulty_level('ttt_button01', USERPATH)")
     ttt_button02 = cmds.iconTextButton("ttt_button02", style='iconOnly',
                                        image=PATH_ICONS + 'ttt_idle.png',
-                                       command="ttt.set_turn('ttt_button02', USERPATH)")
+                                       command="ttt.check_difficulty_level('ttt_button02', USERPATH)")
     ttt_button03 = cmds.iconTextButton("ttt_button03", style='iconOnly',
                                        image=PATH_ICONS + 'ttt_idle.png',
-                                       command="ttt.set_turn('ttt_button03', USERPATH)")
+                                       command="ttt.check_difficulty_level('ttt_button03', USERPATH)")
     ttt_button04 = cmds.iconTextButton("ttt_button04", style='iconOnly',
                                        image=PATH_ICONS + 'ttt_idle.png',
-                                       command="ttt.set_turn('ttt_button04', USERPATH)")
+                                       command="ttt.check_difficulty_level('ttt_button04', USERPATH)")
     ttt_button05 = cmds.iconTextButton("ttt_button05", style='iconOnly',
                                        image=PATH_ICONS + 'ttt_idle.png',
-                                       command="ttt.set_turn('ttt_button05', USERPATH)")
+                                       command="ttt.check_difficulty_level('ttt_button05', USERPATH)")
     ttt_button06 = cmds.iconTextButton("ttt_button06", style='iconOnly',
                                        image=PATH_ICONS + 'ttt_idle.png',
-                                       command="ttt.set_turn('ttt_button06', USERPATH)")
+                                       command="ttt.check_difficulty_level('ttt_button06', USERPATH)")
     ttt_button07 = cmds.iconTextButton("ttt_button07", style='iconOnly',
                                        image=PATH_ICONS + 'ttt_idle.png',
-                                       command="ttt.set_turn('ttt_button07', USERPATH)")
+                                       command="ttt.check_difficulty_level('ttt_button07', USERPATH)")
     ttt_button08 = cmds.iconTextButton("ttt_button08", style='iconOnly',
                                        image=PATH_ICONS + 'ttt_idle.png',
-                                       command="ttt.set_turn('ttt_button08', USERPATH)")
+                                       command="ttt.check_difficulty_level('ttt_button08', USERPATH)")
     ttt_button09 = cmds.iconTextButton("ttt_button09", style='iconOnly',
                                        image=PATH_ICONS + 'ttt_idle.png',
-                                       command="ttt.set_turn('ttt_button09', USERPATH)")
+                                       command="ttt.check_difficulty_level('ttt_button09', USERPATH)")
 
     cmds.formLayout("ttt_formlayout", edit=True,
-                    attachForm=[(ttt_button01, 'left', 25), (ttt_button01, 'top', 25),
-                                (ttt_button02, 'left',180), (ttt_button02, 'top', 25),
-                                (ttt_button03, 'left',335), (ttt_button03, 'top', 25),
-                                (ttt_button04, 'left',25), (ttt_button04, 'top', 180),
-                                (ttt_button05, 'left',180), (ttt_button05, 'top', 180),
-                                (ttt_button06, 'left',335), (ttt_button06, 'top', 180),
-                                (ttt_button07, 'left',25), (ttt_button07, 'top', 335),
-                                (ttt_button08, 'left',180), (ttt_button08, 'top', 335),
-                                (ttt_button09, 'left', 335), (ttt_button09, 'top', 335)])
+         attachForm=[(ttt_button01, 'left', 25), (ttt_button01, 'top', 25),
+                    (ttt_button02, 'left', 180), (ttt_button02, 'top', 25),
+                    (ttt_button03, 'left', 335), (ttt_button03, 'top', 25),
+                    (ttt_button04, 'left', 25), (ttt_button04, 'top', 180),
+                    (ttt_button05, 'left', 180), (ttt_button05, 'top', 180),
+                    (ttt_button06, 'left', 335), (ttt_button06, 'top', 180),
+                    (ttt_button07, 'left', 25), (ttt_button07, 'top', 335),
+                    (ttt_button08, 'left', 180), (ttt_button08, 'top', 335),
+                    (ttt_button09, 'left', 335), (ttt_button09, 'top', 335)])
     cmds.setParent("..")
+
 
     ###############################################################
     #########################   BUTTONS    ########################
@@ -128,3 +133,5 @@ def tic_tac_toe_ui(USERPATH):
                                           image=PATH_ICONS + 'ttt_new_game.png',
                                           command="ttt.restart_game(USERPATH)")
     cmds.showWindow()
+
+
